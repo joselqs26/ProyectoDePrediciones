f06379bb9d2075b72029b69781d39aa95a5b9493
joselqs26 <joseluisquintero06@gmail.com>
2022-05-06 07:45:44 -0500

    Carga y consulta de archivos

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import getcwd
+from fastapi.responses import FileResponse
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
@@ -5,6 +7,14 @@ app = FastAPI()
 async def prueba():
     return "Hola :D"
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+@app.post("/file/")
+async def create_upload_file(file: UploadFile = File(...)):
+    with open( getcwd() + "/" + file.filename, "wb") as myfile:
+        content = await file.read()
+        myfile.write(content)
+        myfile.close() 
+    return "success"
+
+@app.get("/file/{name_file}")
+async def get_file(name_file: str):
+    return FileResponse(getcwd() + "/" + name_file)
